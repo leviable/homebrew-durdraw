f41ad864ba9d2aa8cb530946118a8844f7ec389b
Levi Noecker <11619428+leviable@users.noreply.github.com>
2025-06-19 13:33:55 +0000

    durdraw: add 0.29.0 bottle.

--- Formula/durdraw.rb
+++ Formula/durdraw.rb
@@ -7,6 +7,13 @@ class Durdraw < Formula
   sha256 "7878cc0ed97d03defe01f24935f0fbe18d11c1cfc5c3b801ee12c9116dddf0c5"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://ghcr.io/v2/leviable/durdraw"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6491a7e7ec6dcbbab444906b5aa31c852b38c592fd3fff59f2d45261a376261"
+    sha256 cellar: :any_skip_relocation, ventura:       "218da5bc27ab7daa20b2b3938fce87bdcf467e9bb5fe0bced0fc8408bee6699f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2effaa87f9e144aa5115848be945d7953a51f6c3a903b0d0e3df3b9187488d14"
+  end
+
   depends_on "ansilove"
   depends_on "neofetch"
   depends_on "python@3.13"
